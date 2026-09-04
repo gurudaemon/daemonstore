09b0ca3d7fe78030e5bf9383de49316cf5df93cc
gurudaemon <74869051+gurudaemon@users.noreply.github.com>
2026-09-04 15:43:59 -0400

    admin: atualiza vídeos

--- videos-data.js
+++ videos-data.js
@@ -1,80 +1,96 @@
 const VIDEOS = [
     {
-        id: 1,
-        title: "Documentário Dualidades",
-        type: "documentario",
-        duration: "",
-        year: 2026,
-        thumbnail: "https://img.youtube.com/vi/_253XeW8kHs/maxresdefault.jpg",
-        youtubeId: "_253XeW8kHs",
-        description: "",
-        tags: ["documentario"]
+        "id": 1,
+        "title": "Documentário Dualidades",
+        "type": "documentario",
+        "duration": "",
+        "year": 2026,
+        "thumbnail": "https://img.youtube.com/vi/_253XeW8kHs/maxresdefault.jpg",
+        "youtubeId": "_253XeW8kHs",
+        "description": "",
+        "tags": [
+            "documentario"
+        ]
     },
     {
-        id: 2,
-        title: "Mini Doc. Weedemic",
-        type: "documentario",
-        duration: "",
-        year: 2026,
-        thumbnail: "https://img.youtube.com/vi/0MTd075qIk0/maxresdefault.jpg",
-        youtubeId: "0MTd075qIk0",
-        description: "",
-        tags: ["documentario"]
+        "id": 2,
+        "title": "Mini Doc. Weedemic",
+        "type": "documentario",
+        "duration": "",
+        "year": 2026,
+        "thumbnail": "https://img.youtube.com/vi/0MTd075qIk0/maxresdefault.jpg",
+        "youtubeId": "0MTd075qIk0",
+        "description": "",
+        "tags": [
+            "documentario"
+        ]
     },
     {
-        id: 3,
-        title: "Curta Beto Carrero",
-        type: "curta",
-        duration: "",
-        year: 2026,
-        thumbnail: "https://img.youtube.com/vi/w8MapNJMvrg/maxresdefault.jpg",
-        youtubeId: "w8MapNJMvrg",
-        description: "",
-        tags: ["curta"]
+        "id": 3,
+        "title": "Curta Beto Carrero",
+        "type": "curta",
+        "duration": "",
+        "year": 2026,
+        "thumbnail": "https://img.youtube.com/vi/w8MapNJMvrg/maxresdefault.jpg",
+        "youtubeId": "w8MapNJMvrg",
+        "description": "",
+        "tags": [
+            "curta"
+        ]
     },
     {
-        id: 4,
-        title: "Mr. Jones (Amy Winehouse Cover)",
-        type: "clipe",
-        duration: "",
-        year: 2026,
-        thumbnail: "https://img.youtube.com/vi/OsHBFlc_Yro/maxresdefault.jpg",
-        youtubeId: "OsHBFlc_Yro",
-        description: "",
-        tags: ["clipe", "cover"]
+        "id": 4,
+        "title": "Mr. Jones (Amy Winehouse Cover)",
+        "type": "clipe",
+        "duration": "",
+        "year": 2026,
+        "thumbnail": "https://img.youtube.com/vi/OsHBFlc_Yro/maxresdefault.jpg",
+        "youtubeId": "OsHBFlc_Yro",
+        "description": "",
+        "tags": [
+            "clipe",
+            "cover"
+        ]
     },
     {
-        id: 5,
-        title: "Farol (Cover)",
-        type: "clipe",
-        duration: "",
-        year: 2026,
-        thumbnail: "https://img.youtube.com/vi/i2y8BOFdVw4/maxresdefault.jpg",
-        youtubeId: "i2y8BOFdVw4",
-        description: "",
-        tags: ["clipe", "cover"]
+        "id": 5,
+        "title": "Farol (Cover)",
+        "type": "clipe",
+        "duration": "",
+        "year": 2026,
+        "thumbnail": "https://img.youtube.com/vi/i2y8BOFdVw4/maxresdefault.jpg",
+        "youtubeId": "i2y8BOFdVw4",
+        "description": "",
+        "tags": [
+            "clipe",
+            "cover"
+        ]
     },
     {
-        id: 6,
-        title: "Boku ga Kira da",
-        type: "clipe",
-        duration: "",
-        year: 2026,
-        thumbnail: "https://img.youtube.com/vi/JzlCDIxsEOU/maxresdefault.jpg",
-        youtubeId: "JzlCDIxsEOU",
-        description: "",
-        tags: ["clipe"]
+        "id": 6,
+        "title": "Boku ga Kira da",
+        "type": "clipe",
+        "duration": "",
+        "year": 2026,
+        "thumbnail": "https://img.youtube.com/vi/JzlCDIxsEOU/maxresdefault.jpg",
+        "youtubeId": "JzlCDIxsEOU",
+        "description": "",
+        "tags": [
+            "clipe"
+        ]
     },
     {
-        id: 7,
-        title: "Vital",
-        type: "clipe",
-        duration: "",
-        year: 2026,
-        thumbnail: "https://img.youtube.com/vi/nSWbMdv5JsU/maxresdefault.jpg",
-        youtubeId: "nSWbMdv5JsU",
-        description: "",
-        tags: ["clipe"]
+        "id": 7,
+        "title": "Vital",
+        "type": "clipe",
+        "duration": "",
+        "year": 2026,
+        "thumbnail": "https://img.youtube.com/vi/nSWbMdv5JsU/maxresdefault.jpg",
+        "youtubeId": "nSWbMdv5JsU",
+        "description": "",
+        "tags": [
+            "clipe"
+        ]
     }
 ];
 
